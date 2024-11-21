cd4bc245e8a62fce870fa8b6ef1ceeed3a8b49b3
Will <vini-np7@hotmail.com>
2024-11-21 20:57:41 -0300

    create table products in dynamodb

--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -2,16 +2,30 @@ import * as lambda from "aws-cdk-lib/aws-lambda"
 import * as lambdaNodeJs from "aws-cdk-lib/aws-lambda-nodejs"
 import * as cdk from "aws-cdk-lib"
 import { Construct } from "constructs"
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
 
 export class ProductsAppStack extends cdk.Stack {
 
   // create function lambda
   readonly productsFetchHandler: lambdaNodeJs.NodejsFunction
+  readonly productsDdb: dynamodb.Table
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
-    this.productsFetchHandler = new lambdaNodeJs.NodejsFunction(this, 
+    this.productsDdb = new dynamodb.Table(this, "ProductsDdb", {
+      tableName: "products",
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      partitionKey: {
+        name: 'id',
+        type: dynamodb.AttributeType.STRING
+      },
+      billingMode: dynamodb.BillingMode.PROVISIONED,
+      readCapacity: 1,
+      writeCapacity: 1
+    })
+
+    this.productsFetchHandler = new lambdaNodeJs.NodejsFunction(this,
       "ProductsFetchFunction",
       {
         functionName: "ProductsFetchFunction",             // nome da function que vai aparecer na AWS
@@ -19,7 +33,7 @@ export class ProductsAppStack extends cdk.Stack {
         handler: "handler",                                // metodo que vai ser executado
         memorySize: 512,                                   // memoria maxima alocada para essa função 
         timeout: cdk.Duration.seconds(5),                  // tempo maximo de execução
-        bundling:{
+        bundling: {
           minify: true,                                    // minificando o arquivo bruto para fazer o deploy
           sourceMap: false                                 // desabilita o debug
         },
